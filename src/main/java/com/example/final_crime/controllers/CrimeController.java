35f1f4e42a1d0033ecfa11245709cd437c885685
kwon seok kyeong <seedksk@gmail.com>
2023-11-17 16:40:09 +0900

    audio upload add

--- src/main/java/com/example/final_crime/controllers/CrimeController.java
+++ src/main/java/com/example/final_crime/controllers/CrimeController.java
@@ -9,8 +9,20 @@ import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +39,105 @@ public class CrimeController {
         return "hi";
     }
 
+    private static final String UPLOAD_DIR = "uploads";
+
+    @PostMapping("/audioUpload")
+    public ResponseEntity<String> handleAudioUpload(@RequestBody MultipartFile audioFile) {
+        if (audioFile.isEmpty()) {
+            return new ResponseEntity<>("업로드된 파일이 없습니다.", HttpStatus.BAD_REQUEST);
+        }
+        System.out.println(audioFile.toString());
+        try {
+            // 업로드된 PCM 파일을 WAV로 변환하여 저장합니다.
+            byte[] wavData = convertPCMtoWAV(audioFile.getBytes());
+            
+            // WAV 파일을 저장할 디렉토리를 설정합니다.
+            Path uploadPath = Paths.get(UPLOAD_DIR);
+            String filePath = uploadPath.toAbsolutePath().toString() + File.separator + audioFile.getOriginalFilename() + ".wav";
+
+            // 디렉토리가 없으면 생성합니다.
+            File dir = new File(uploadPath.toAbsolutePath().toString());
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // WAV 파일을 저장합니다.
+            try (OutputStream outputStream = new FileOutputStream(filePath)) {
+                outputStream.write(wavData);
+            }
+            System.out.println(wavData.toString());
+            System.out.println(filePath);
+
+            return new ResponseEntity<>("오디오 파일이 성공적으로 업로드되었습니다.", HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("오디오 파일 업로드 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    public static byte[] generateWavHeader(byte[] pcmData, int sampleRate, int channels, int bitsPerSample) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        // WAV 헤더 생성
+        writeString(outputStream, "RIFF"); // ChunkID
+        writeInt(outputStream, 36 + pcmData.length); // ChunkSize
+        writeString(outputStream, "WAVE"); // Format
+
+        writeString(outputStream, "fmt "); // Subchunk1ID
+        writeInt(outputStream, 16); // Subchunk1Size
+        writeShort(outputStream, (short) 1); // AudioFormat (PCM)
+        writeShort(outputStream, (short) channels); // NumChannels
+        writeInt(outputStream, sampleRate); // SampleRate
+        writeInt(outputStream, sampleRate * channels * bitsPerSample / 8); // ByteRate
+        writeShort(outputStream, (short) (channels * bitsPerSample / 8)); // BlockAlign
+        writeShort(outputStream, (short) bitsPerSample); // BitsPerSample
+
+        writeString(outputStream, "data"); // Subchunk2ID
+        writeInt(outputStream, pcmData.length); // Subchunk2Size
+
+        // PCM 데이터 추가
+        outputStream.write(pcmData);
+
+        return outputStream.toByteArray();
+    }
+
+    private static void writeString(ByteArrayOutputStream stream, String value) throws IOException {
+        byte[] bytes = value.getBytes();
+        stream.write(bytes);
+    }
+
+    private static void writeInt(ByteArrayOutputStream stream, int value) throws IOException {
+        byte[] bytes = ByteBuffer.allocate(4).putInt(value).array();
+        stream.write(bytes);
+    }
+
+    private static void writeShort(ByteArrayOutputStream stream, short value) throws IOException {
+        byte[] bytes = ByteBuffer.allocate(2).putShort(value).array();
+        stream.write(bytes);
+    }
+
+    private byte[] convertPCMtoWAV(byte[] pcmData) throws IOException {
+        // 여기에 PCM 데이터를 WAV로 변환하는 코드를 작성하세요.
+        // 예제로 진행하려면 추가 라이브러리를 사용하거나 WAV 파일 형식에 대한 자세한 이해가 필요합니다.
+        // 여기서는 간단한 예제를 제공하고 있습니다.
+
+        // 예제: 간단한 WAV 헤더 추가
+        int dataSize = pcmData.length;
+        int totalSize = 36 + dataSize;  // WAV 헤더 크기 (36 바이트) + PCM 데이터 크기
+
+        //byte[] wavData = new byte[totalSize];
+
+        // WAV 헤더 추가
+        // 여기에 WAV 헤더를 생성하고 wavData에 추가하는 코드를 작성하세요.\
+        
+        byte[] wavData = generateWavHeader(pcmData, 44100, 1, 16);
+
+        // PCM 데이터 추가
+        System.arraycopy(pcmData, 0, wavData, 36, dataSize);
+
+        return wavData;
+    }
+    
     @GetMapping("/allOffice")
     public JsonArray getOffice() throws Exception {
         List<OfficeDTO> list = crimeService.allOffice();
